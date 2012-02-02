42e212faec38ec721d60fe66bc9ae6904065b36f
Daichi TOMA <amothic@gmail.com>
2012-02-03 00:18:40 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gohan::Application.config.secret_token = 'd4a95179ca8fe2929ac24cc3a9cc87e8ff540720820583a4b1a4ee5c957d036bba5804d26c70fca8603aa006f0a655fe6c72f68cd97201feb154a33e8b26eb8b'
